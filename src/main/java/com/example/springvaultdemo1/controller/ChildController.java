198b1168aa7ccf123303e958ca16227b427da80b
MR Ferry <ferry.suhandri@pji-solutions.com>
2022-04-02 22:34:35 +0700

    feat: encrypt/decrypt file

--- src/main/java/com/example/springvaultdemo1/controller/ChildController.java
+++ src/main/java/com/example/springvaultdemo1/controller/ChildController.java
@@ -3,10 +3,17 @@ package com.example.springvaultdemo1.controller;
 import com.example.springvaultdemo1.entity.Child;
 import com.example.springvaultdemo1.repo.ChildRepo;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.vault.core.VaultOperations;
 import org.springframework.vault.support.Ciphertext;
 import org.springframework.vault.support.Plaintext;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /************************
  * Made by [MR Ferry™]  *
@@ -23,7 +30,8 @@ public class ChildController{
 	Child save(@RequestBody Child childReq){
 		Child child = new Child();
 		child.setName(childReq.getName());
-		Ciphertext encryptedParent = vaultOperations.opsForTransit().encrypt("nik", Plaintext.of(childReq.getParentName()));
+		Ciphertext encryptedParent = vaultOperations.opsForTransit()
+				.encrypt("nik", Plaintext.of(childReq.getParentName()));
 		child.setParentName(encryptedParent.getCiphertext());
 		return childRepo.save(child);
 	}
@@ -31,13 +39,38 @@ public class ChildController{
 	@GetMapping("get")
 	Child get(@RequestParam int id){
 		Child child = childRepo.getById(id);
-		Plaintext decryptedParent = vaultOperations.opsForTransit().decrypt("nik", Ciphertext.of(child.getParentName()));
+		Plaintext decryptedParent = vaultOperations.opsForTransit()
+				.decrypt("nik", Ciphertext.of(child.getParentName()));
 
 		Child result = new Child();
 		result.setId(child.getId());
 		result.setName(child.getName());
 		result.setParentName(decryptedParent.asString());
 		return result;
+	}
+
+	@SneakyThrows
+	@GetMapping("encryptFile")
+	void encryptFile(){
+		File file = new File("pom.xml");
+		byte[] bytes = Files.readAllBytes(file.toPath());
+		Ciphertext encrypt = vaultOperations.opsForTransit()
+				.encrypt("nik", Plaintext.of(bytes));
+		byte[] context = encrypt.getCiphertext().getBytes(StandardCharsets.UTF_8);
+		OutputStream outputStream = new FileOutputStream(new File("pom.enc"));
+		outputStream.write(context);
+	}
+
+	@SneakyThrows
+	@GetMapping("decryptFile")
+	void decryptFile(){
+		File file = new File("pom.enc");
+		String string = Files.readString(file.toPath());
+		Plaintext encrypt = vaultOperations.opsForTransit()
+				.decrypt("nik", Ciphertext.of(string));
+		byte[] context = encrypt.getPlaintext();
+		OutputStream outputStream = new FileOutputStream(new File("pom.dec"));
+		outputStream.write(context);
 	}
 
 }
